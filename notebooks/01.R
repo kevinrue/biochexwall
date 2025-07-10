3dd638bb65eb65198c69d5288354fcc5247368b9
Kevin Rue <kevinrue@users.noreply.github.com>
2025-07-10 11:05:25 +0100

    sort stickers by average colour

--- notebooks/01.R
+++ notebooks/01.R
@@ -83,9 +83,12 @@ ggsave("img/outputs/biochexwall.png", gg, width = 20, height = 20, dpi = 600)
 sticker_files <- list.files("img/stickers_cropped", full.names = TRUE, pattern = "\\.png$")
 # sticker_files <- sticker_files[!grepl("CSAMA2019", sticker_files)]
 
+sticker_files_pool <- rep(sticker_files, 1 + nrow(image_xy_coords_downsampled) %/% length(sticker_files))
+sticker_files_pool <- sticker_files_pool[1:nrow(image_xy_coords_downsampled)]
+
 final_df <- data.frame(
   image_xy_coords_downsampled,
-  image = c(sticker_files, sticker_files[1:(nrow(image_xy_coords_downsampled) - length(sticker_files))])
+  image = sticker_files_pool[1:nrow(image_xy_coords_downsampled)]
 )
 
 gg <- ggplot(final_df, aes(col, row)) +
@@ -98,3 +101,51 @@ gg <- ggplot(final_df, aes(col, row)) +
   )
 ggsave("img/outputs/biochexwall-mixed-600.png", gg, width = 20, height = 20, dpi = 600)
 
+# for each sticker in the pool, identify the predominant color
+get_mean_colour <- function(file) {
+  message(file)
+  nuc = readImage(file)
+  # display(nuc)
+  nuc_data = imageData(nuc)[, , 1:3]
+  # nuc_data is a three dimensional array
+  # convert it into a data.frame of five columns: x, y, r, g, b
+  nuc_df <- expand.grid(row = 1:nrow(nuc_data), col = 1:ncol(nuc_data))
+  nuc_df <- nuc_df[seq(1, nrow(nuc_df), by = 10), ]
+  nuc_df[, "R"] <- mapply(function(row, col) nuc_data[row, col, 1], row = nuc_df$row, col = nuc_df$col)
+  nuc_df[, "G"] <- mapply(function(row, col) nuc_data[row, col, 2], row = nuc_df$row, col = nuc_df$col)
+  nuc_df[, "B"] <- mapply(function(row, col) nuc_data[row, col, 3], row = nuc_df$row, col = nuc_df$col)
+  # nuc_df <- subset(nuc_df, R + G + B < 3)  # remove white pixels
+  mean_colour <- colMeans(nuc_df[, c("R", "G", "B")])
+  rgb <- rgb(mean_colour[1], mean_colour[2], mean_colour[3])
+  message(rgb)
+  rgb
+}
+
+mean_colours <- sapply(sticker_files, get_mean_colour, USE.NAMES = TRUE)
+
+library(TSP)
+rgb <- col2rgb(mean_colours)
+tsp <- as.TSP(dist(t(rgb)))
+sol <- solve_TSP(tsp, control = list(repetitions = 1e3))
+ordered_cols <- mean_colours[sol]
+
+# plot(x = 1:length(ordered_cols), y = rep(1, length(ordered_cols)), col = ordered_cols)
+
+ordered_stickers <- names(ordered_cols)
+ordered_stickers_pool <- rep(ordered_stickers, 1 + nrow(image_xy_coords_downsampled) %/% length(ordered_stickers))
+ordered_stickers_pool <- ordered_stickers_pool[1:nrow(image_xy_coords_downsampled)]
+
+final_df <- data.frame(
+  image_xy_coords_downsampled,
+  image = ordered_stickers_pool[1:nrow(image_xy_coords_downsampled)]
+)
+
+gg <- ggplot(final_df, aes(col, row)) +
+  geom_image(aes(image = image), size = 0.015) +
+  coord_fixed() +
+  labs(x = NULL, y = NULL) +
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = "white", color = NA)
+  )
+ggsave("img/outputs/biochexwall-sorted-600dpi.png", gg, width = 20, height = 20, dpi = 600)
