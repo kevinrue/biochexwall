b2ed9cd4ae91398d41c5fc19590d4b869ece934c
Kevin Rue <kevinrue@users.noreply.github.com>
2025-07-10 08:20:33 +0100

    homemade convex hull based background cropping

--- notebooks/crop_stickers.R
+++ notebooks/crop_stickers.R
@@ -1,3 +1,5 @@
+library(geometry)
+
 sticker_files <- list.files("img/stickers", full.names = TRUE, pattern = "\\.png$")
 
 # library(EBImageExtra)
@@ -15,13 +17,18 @@ for (infile in sticker_files) {
   }
   message("Processing ", infile)
   nuc = readImage(infile)
-  
+  # display(nuc)
   if (dim(nuc)[3] == 3) {
     nuc <- Image(abind(nuc, array(1, dim = c(dim(nuc)[1], dim(nuc)[2], 1)), along = 3), colormode = "Color")
   }
-
+  
   sum_rgb <- rowSums(nuc[, , 1:3], dims = 2)
-  bg_idx <- which(sum_rgb == 3, arr.ind = TRUE)
+  bg_colour <- ifelse(sum_rgb[1,1] == 0, "black", "white")
+  if (bg_colour == "black") {
+    bg_idx <- which(sum_rgb == 0, arr.ind = TRUE)
+  } else {
+    bg_idx <- which(sum_rgb == 3, arr.ind = TRUE)
+  }
   
   # use row and column indices in bg_idx to set those pixels to transparent
   for (i in seq_len(nrow(bg_idx))) {
@@ -29,9 +36,46 @@ for (infile in sticker_files) {
     col <- bg_idx[i, "col"]
     nuc[row, col, 4] <- 0  # set alpha channel to 0 for background pixels
   }
-  
-  # nuc[bg_idx[,"row"],bg_idx[,"col"],4] <- 0  # set alpha channel to 0 for background pixels
   # display(nuc)
+  
+  # identify coordinates of convex hull of solid pixels (where alpha channel is 1)
+  coords_solid <- which(imageData(nuc)[,,4] == 1, arr.ind = TRUE)
+  # ggplot(as.data.frame(coords_solid), aes(row, -col)) +
+  #   geom_point() +
+  #   labs(x = "Columns", y = "Rows", title = "Scatter Plot of Solid Pixels") +
+  #   coord_fixed()
+  # chull_keep <- chull(coords_solid)
+  # coords_chull <- coords_solid[chull_keep, ]
+  coords_chull <- convhulln(coords_solid)
+  coords_transparent <- which(imageData(nuc)[,,4] == 0, arr.ind = TRUE)
+  storage.mode(coords_transparent) <- "double"
+  coors_to_reset <- coords_transparent[geometry::inhulln(coords_chull, coords_transparent),]
+  # ggplot(as.data.frame(coors_to_reset), aes(row, -col)) +
+  #   geom_point() +
+  #   labs(x = "Columns", y = "Rows", title = "Scatter Plot of Solid Pixels") +
+  #   coord_fixed()
+  
+  for (i in seq_len(nrow(coors_to_reset))) {
+    row <- coors_to_reset[i, "row"]
+    col <- coors_to_reset[i, "col"]
+    nuc[row, col, 4] <- 1  # restore alpha channel to 1 for pixels within convex hull
+  }
+  
+  # display(nuc)
+  
+  # plot(coords_solid, type = "l", col = "red", lwd = 2, main = "Convex Hull of Solid Pixels")
+  
+  # image_data_plot <- t(image_data)[seq(nrow(image_data), 1, -1), ]
+  # heatmap(image_data_plot,
+  #         Rowv = NA,
+  #         Colv = NA,
+  #         col = gray.colors(256),
+  #         scale = "none",
+  #         xlab = "Columns",
+  #         ylab = "Rows",
+  #         main = "Heatmap of Image Data")
   
   writeImage(nuc, output_file, type = "png")
 }
+
+
